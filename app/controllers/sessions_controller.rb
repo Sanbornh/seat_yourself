d62f6b695ac4eb74cf373c85fbc0b6e2b4e9c265
Sanborn <sanbornh@rogers.com>
2014-04-21 19:22:01 -0400

    User can now create a session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+  	binding.pry
+  	user = User.find_by_email(params[:email])
+  	if user && user.authenticate(params[:password])
+  		session[:user_id] = user.id
+  		redirect_to reservation_path(user)
+  	else
+  		render :new, flash[:notice] => "Oops, something went wrong."
+  	end
   end
 
   def destroy
   end
+
+  
 end
